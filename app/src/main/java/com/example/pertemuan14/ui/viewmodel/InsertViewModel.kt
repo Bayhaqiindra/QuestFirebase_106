dd04aebf56df36bb33821ed2d7f13211110647ea
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-12 01:08:49 +0700

    Menambahkan data class FormErrorState untuk menangani error state

--- app/src/main/java/com/example/pertemuan14/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/viewmodel/InsertViewModel.kt
@@ -2,6 +2,20 @@ package com.example.pertemuan14.ui.viewmodel
 
 import com.example.pertemuan14.model.Mahasiswa
 
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val gender: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && gender == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
 data class MahasiswaEvent(
     val nim: String = "",
     val nama: String = "",
