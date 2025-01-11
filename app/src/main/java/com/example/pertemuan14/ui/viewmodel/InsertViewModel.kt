6ac32320cfb18ad7d4a61b29790bc4c7bb514a72
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-12 01:03:45 +0700

    Menambahkan data class MahasiswaEvent untuk menyimpan informasi mahasiswa

--- app/src/main/java/com/example/pertemuan14/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/viewmodel/InsertViewModel.kt
@@ -2,11 +2,20 @@ package com.example.pertemuan14.ui.viewmodel
 
 import com.example.pertemuan14.model.Mahasiswa
 
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
 fun MahasiswaEvent.toMhsModel() : Mahasiswa = Mahasiswa (
     nim = nim,
     nama = nama,
-    jenisKelamin = jenisKelamin,
     alamat = alamat,
+    gender = gender,
     kelas = kelas,
     angkatan = angkatan
 )
