c1d8647f433956de74e9bf78d49bd341033822a9
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-12 05:48:59 +0700

    fixed code

--- app/src/main/java/com/example/pertemuan14/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pertemuan14/ui/viewmodel/InsertViewModel.kt
@@ -54,48 +54,55 @@ class InsertViewModel (
         }
     }
 
-    sealed class FormState {
-        object Idle : FormState()
-        object Loading : FormState()
-        data class Success(val message: String) : FormState()
-        data class Error(val message: String) : FormState()
-
+    fun resetForm(){
+        uiEvent = InsertUiState()
+        uiState = FormState.Idle
     }
 
-    data class InsertUiState(
-        val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
-        val isEntryValid: FormErrorState = FormErrorState(),
-    )
-
-    data class FormErrorState(
-        val nim: String? = null,
-        val nama: String? = null,
-        val gender: String? = null,
-        val alamat: String? = null,
-        val kelas: String? = null,
-        val angkatan: String? = null
-    ) {
-        fun isValid(): Boolean {
-            return nim == null && nama == null && gender == null &&
-                    alamat == null && kelas == null && angkatan == null
-        }
+    fun resetSnackBarMessage(){
+        uiState = FormState.Idle
     }
-
-    data class MahasiswaEvent(
-        val nim: String = "",
-        val nama: String = "",
-        val alamat: String = "",
-        val gender: String = "",
-        val kelas: String = "",
-        val angkatan: String = ""
-    )
-
-    fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
-        nim = nim,
-        nama = nama,
-        alamat = alamat,
-        gender = gender,
-        kelas = kelas,
-        angkatan = angkatan
-    )
 }
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Success(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+    }
+
+data class InsertUiState(
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+    )
+
+data class FormErrorState(
+    val nim: String? = null,
+    val nama: String? = null,
+    val gender: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid(): Boolean {
+        return nim == null && nama == null && gender == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
+data class MahasiswaEvent(
+    val nim: String = "",
+    val nama: String = "",
+    val alamat: String = "",
+    val gender: String = "",
+    val kelas: String = "",
+    val angkatan: String = ""
+)
+
+fun MahasiswaEvent.toMhsModel(): Mahasiswa = Mahasiswa(
+    nim = nim,
+    nama = nama,
+    alamat = alamat,
+    gender = gender,
+    kelas = kelas,
+    angkatan = angkatan
+)
