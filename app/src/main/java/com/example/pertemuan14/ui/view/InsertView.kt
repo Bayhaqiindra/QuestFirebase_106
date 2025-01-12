2c755431f641ffbcc00e38550effe7ba04f55981
Bayhaqiindra <127074133+Bayhaqiindra@users.noreply.github.com>
2025-01-12 19:47:48 +0700

    Fixed topappbar

--- app/src/main/java/com/example/pertemuan14/ui/view/InsertView.kt
+++ app/src/main/java/com/example/pertemuan14/ui/view/InsertView.kt
@@ -10,15 +10,19 @@ import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
 import androidx.compose.material3.CircularProgressIndicator
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.SnackbarHost
 import androidx.compose.material3.SnackbarHostState
 import androidx.compose.material3.Text
-import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.remember
@@ -29,6 +33,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.pertemuan14.ui.customwidget.TopAppBar
 import com.example.pertemuan14.ui.viewmodel.FormErrorState
 import com.example.pertemuan14.ui.viewmodel.FormState
 import com.example.pertemuan14.ui.viewmodel.InsertUiState
@@ -38,7 +43,7 @@ import com.example.pertemuan14.ui.viewmodel.PenyediaViewModel
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
-
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun InsertMhsView(
     onBack: () -> Unit,
@@ -81,12 +86,10 @@ fun InsertMhsView(
         snackbarHost = { SnackbarHost(hostState = snackbarHostState) },
         topBar = {
             TopAppBar(
-                title = { Text("Tambah Mahasiswa") },
-                navigationIcon = {
-                    Button(onClick = onBack) {
-                        Text("Back")
-                    }
-                }
+                judul = "Tambah Mahasiswa",
+                showBackButton = true,
+                onBack = onBack,
+                modifier = modifier
             )
         }
     ) { padding ->
@@ -197,8 +200,6 @@ fun FormMahasiswa(
             color = Color.Red
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
-
         Text(text = "Jenis Kelamin")
         Row (
             modifier = Modifier.fillMaxWidth()
@@ -239,7 +240,6 @@ fun FormMahasiswa(
             text = errorState.alamat?: "",
             color = Color.Red
         )
-        Spacer(modifier = Modifier.height(16.dp))
 
         Text(text = "Kelas")
         Row (
@@ -267,7 +267,6 @@ fun FormMahasiswa(
             color = Color.Red
         )
 
-        Spacer(modifier = Modifier.height(16.dp))
         OutlinedTextField(
             modifier = Modifier.fillMaxWidth(),
             value = mahasiswaEvent.angkatan,
